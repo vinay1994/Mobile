2affceab2b86acbdf68cccf114a9ce30187cd76f
BattaKanakaDurga <durgashanvi1234@gmail.com>
2018-09-24 11:10:09 +0530

    multiple user login

--- FlipApp/src/test/java/com/fliplearn/flipapp/testcases/Quiz.java
+++ FlipApp/src/test/java/com/fliplearn/flipapp/testcases/Quiz.java
@@ -1,5 +1,39 @@
 package com.fliplearn.flipapp.testcases;
 
-public class Quiz {
+import org.testng.annotations.Test;
 
+import com.fliplearn.flipapp.helper.Base;
+import com.fliplearn.flipapp.pagemodules.LoginModule;
+import com.fliplearn.flipapp.pagemodules.MenuModule;
+import com.fliplearn.flipapp.pagemodules.OnboardingModule;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class Quiz extends Base 
+{
+
+	LoginModule logMod;
+	OnboardingModule onbMod;
+	MenuModule menuMod;
+
+	/**
+	 * Verify Quiz on Android App
+	 * @author Durga
+	 * @since 2018-09-21
+	 * @version 1.0
+	 */
+	@Test
+	public void verifyAndroidQuiz()
+	{
+		logMod = new LoginModule(driver);
+		onbMod = new OnboardingModule(driver);
+		menuMod = new MenuModule(driver);
+		
+		onbMod.skipScreen();
+		logMod.Login("admin");		
+		((AndroidDriver) driver).hideKeyboard();
+		menuMod.menuBtn.click();
+		
+	}
+	
 }
--- FlipApp/src/test/java/com/fliplearn/flipapp/testcases/UserRegistration.java
+++ FlipApp/src/test/java/com/fliplearn/flipapp/testcases/UserRegistration.java
@@ -41,6 +41,142 @@ public class UserRegistration extends Base
 		onbMod = new OnboardingModule(driver);
 		
 		onbMod.skipScreen();
+		logMod.Login("admin");		
+		((AndroidDriver) driver).hideKeyboard();
+		logMod.loginBtn.click();
+		
+	}
+	
+	/**
+	 * Verify User Principal Login on Desktop Browser
+	 * @author Durga
+	 * @since 2018-09-21
+	 * @version 1.0
+	 */
+	@Test
+	public void verifyDesktopPrincipalLogin()
+	{
+		logMod = new LoginModule(driver);
+		logMod.Login("principal");
+		logMod.loginBtn.click();
+	}
+	
+	/**
+	 * Verify Principal User Login on Android App
+	 * @author Durga
+	 * @since 2018-09-21
+	 * @version 1.0
+	 */
+	@Test
+	public void verifyAndroidPrincipalLogin()
+	{
+		logMod = new LoginModule(driver);
+		onbMod = new OnboardingModule(driver);
+		
+		onbMod.skipScreen();
+		logMod.Login("principal");		
+		((AndroidDriver) driver).hideKeyboard();
+		logMod.loginBtn.click();
+		
+	}
+	
+	
+	/**
+	 * Verify User Teacher Login on Desktop Browser
+	 * @author Durga
+	 * @since 2018-09-21
+	 * @version 1.0
+	 */
+	@Test
+	public void verifyDesktopTeacherLogin()
+	{
+		logMod = new LoginModule(driver);
+		logMod.Login("teacher");
+		logMod.loginBtn.click();
+	}
+	
+	/**
+	 * Verify Teacher User Login on Android App
+	 * @author Durga
+	 * @since 2018-09-21
+	 * @version 1.0
+	 */
+	@Test
+	public void verifyAndroidTeacherLogin()
+	{
+		logMod = new LoginModule(driver);
+		onbMod = new OnboardingModule(driver);
+		
+		onbMod.skipScreen();
+		logMod.Login("teacher");		
+		((AndroidDriver) driver).hideKeyboard();
+		logMod.loginBtn.click();
+		
+	}	
+	
+	
+	/**
+	 * Verify User Parent Login on Desktop Browser
+	 * @author Durga
+	 * @since 2018-09-21
+	 * @version 1.0
+	 */
+	@Test
+	public void verifyDesktopParentLogin()
+	{
+		logMod = new LoginModule(driver);
+		logMod.Login("parent");
+		logMod.loginBtn.click();
+	}
+	
+	/**
+	 * Verify Parent User Login on Android App
+	 * @author Durga
+	 * @since 2018-09-21
+	 * @version 1.0
+	 */
+	@Test
+	public void verifyAndroidParentLogin()
+	{
+		logMod = new LoginModule(driver);
+		onbMod = new OnboardingModule(driver);
+		
+		onbMod.skipScreen();
+		logMod.Login("parent");		
+		((AndroidDriver) driver).hideKeyboard();
+		logMod.loginBtn.click();
+		
+	}
+
+	
+	
+	/**
+	 * Verify User Student Login on Desktop Browser
+	 * @author Durga
+	 * @since 2018-09-21
+	 * @version 1.0
+	 */
+	@Test
+	public void verifyDesktopStudentLogin()
+	{
+		logMod = new LoginModule(driver);
+		logMod.Login("student");
+		logMod.loginBtn.click();
+	}
+	
+	/**
+	 * Verify Student User Login on Android App
+	 * @author Durga
+	 * @since 2018-09-21
+	 * @version 1.0
+	 */
+	@Test
+	public void verifyAndroidStudentLogin()
+	{
+		logMod = new LoginModule(driver);
+		onbMod = new OnboardingModule(driver);
+		
+		onbMod.skipScreen();
 		logMod.Login("student");		
 		((AndroidDriver) driver).hideKeyboard();
 		logMod.loginBtn.click();
