5f211e1a0cd0548bea6ec25bfc63f4abc6bbf57e
vinay <vinay.yadav@fliplearn.com>
2018-10-22 16:46:25 +0530

    added fee test cases

---
+++ FlipApp/src/test/java/com/fliplearn/flipapp/testcases/Fee.java
@@ -0,0 +1,175 @@
+package com.fliplearn.flipapp.testcases;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.fliplearn.flipapp.helper.Base;
+import com.fliplearn.flipapp.pagemodules.FeeModule;
+import com.fliplearn.flipapp.pagemodules.LoginModule;
+
+public class Fee extends Base {
+	FeeModule feeMod;
+	LoginModule logMod;
+	Base base;
+	String schoolName;
+	@BeforeMethod
+	public void beforMethodFee() {
+		feeMod=new FeeModule(driver);
+		logMod = new LoginModule(driver);
+		base=new Base();
+		
+	}
+	
+	/**
+	 * Verify fee without login by selecting debit card on Web, Android and iOS
+	 * @author vinay kumar 
+	 * @since 2018-09-30
+	 * @version 1.2
+	 * @throws Throwable 
+	 */
+	
+	@Test
+	public void payFeeWithoutLoginSelectedByDebitCard() throws IOException, InterruptedException {
+		base.readData(platform, "Guest", schoolName);
+		feeMod.clickOnPayfeeMod();
+		feeMod.selectSchoolToPayFee("Qa dashboard testing");
+		feeMod.enterAdmission("1994");
+		feeMod.clickOnProceedbtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPopUp(),feeMod.getActualFeeOnPopUp());
+		feeMod.fillAddress();
+		feeMod.selectDebitCard();
+		Assert.assertEquals(feeMod.getInternetPlusTotalOnAddressPage(), feeMod.gettotalFeeOnAddressPage());
+		Assert.assertEquals(feeMod.gettotalFeeOnAddressPage(),feeMod.getTotalFeeOnPayBtn());
+		feeMod.clickOnPayBtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPayBtn(),feeMod.getTotalFeeOnPaymentGat());
+		feeMod.clickOncancelBtn();
+
+	}
+	/**
+	 * Verify fee without login by selecting net banking  on Web, Android and iOS
+	 * @author vinay kumar 
+	 * @since 2018-09-30
+	 * @version 1.2
+	 * @throws Throwable 
+	 */
+	
+	@Test
+	public void payFeeWithoutLoginSelectedByNetBnking() throws IOException, InterruptedException {
+		base.readData(platform, "Guest", schoolName);
+		feeMod.clickOnPayfeeMod();
+		feeMod.selectSchoolToPayFee("Qa dashboard testing");
+		feeMod.enterAdmission("1994");
+		feeMod.clickOnProceedbtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPopUp(),feeMod.getActualFeeOnPopUp());
+		feeMod.fillAddress();
+		feeMod.selectNetbanking();
+		Assert.assertEquals(feeMod.getInternetPlusTotalOnAddressPage(), feeMod.gettotalFeeOnAddressPage());
+		Assert.assertEquals(feeMod.gettotalFeeOnAddressPage(),feeMod.getTotalFeeOnPayBtn());
+		feeMod.clickOnPayBtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPayBtn(),feeMod.getTotalFeeOnPaymentGat());
+		feeMod.clickOncancelBtn();
+
+	}
+	
+	/**
+	 * Verify fee without login by selecting upi  on Web, Android and iOS
+	 * @author vinay kumar 
+	 * @since 2018-09-30
+	 * @version 1.2
+	 * @throws Throwable 
+	 */
+	@Test
+	public void payFeeWithoutLoginSelectedByUpi() throws IOException, InterruptedException {
+		base.readData(platform, "Guest", schoolName);
+		feeMod.clickOnPayfeeMod();
+		feeMod.selectSchoolToPayFee("Qa dashboard testing");
+		feeMod.enterAdmission("1994");
+		feeMod.clickOnProceedbtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPopUp(),feeMod.getActualFeeOnPopUp());
+		feeMod.fillAddress();
+		feeMod.selectUpi();
+		Assert.assertEquals(feeMod.getInternetPlusTotalOnAddressPage(), feeMod.gettotalFeeOnAddressPage());
+		Assert.assertEquals(feeMod.gettotalFeeOnAddressPage(),feeMod.getTotalFeeOnPayBtn());
+		feeMod.clickOnPayBtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPayBtn(),feeMod.getTotalFeeOnPaymentGat());
+		feeMod.clickOncancelBtn();
+
+	}
+	/**
+	 * Verify fee without login by selecting visa/master card on Web, Android and iOS
+	 * @author vinay kumar 
+	 * @since 2018-09-30
+	 * @version 1.2
+	 * @throws Throwable 
+	 */
+	@Test
+	public void payFeeWithoutLoginSelectedByVisa() throws IOException, InterruptedException {
+		base.readData(platform, "Guest", schoolName);
+		feeMod.clickOnPayfeeMod();
+		feeMod.selectSchoolToPayFee("Qa dashboard testing");
+		feeMod.enterAdmission("1994");
+		feeMod.clickOnProceedbtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPopUp(),feeMod.getActualFeeOnPopUp());
+		feeMod.fillAddress();
+		feeMod.selectVisa();
+		Assert.assertEquals(feeMod.getInternetPlusTotalOnAddressPage(), feeMod.gettotalFeeOnAddressPage());
+		Assert.assertEquals(feeMod.gettotalFeeOnAddressPage(),feeMod.getTotalFeeOnPayBtn());
+		feeMod.clickOnPayBtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPayBtn(),feeMod.getTotalFeeOnPaymentGat());
+		feeMod.clickOncancelBtn();
+
+	}
+	/**
+	 * Verify fee without login by selecting Amex card on Web, Android and iOS
+	 * @author vinay kumar 
+	 * @since 2018-09-30
+	 * @version 1.2
+	 * @throws Throwable 
+	 */
+	@Test
+	public void payFeeWithoutLoginSelectedByAmex() throws IOException, InterruptedException {
+		base.readData(platform, "Guest", schoolName);
+		feeMod.clickOnPayfeeMod();
+		feeMod.selectSchoolToPayFee("Qa dashboard testing");
+		feeMod.enterAdmission("1994");
+		feeMod.clickOnProceedbtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPopUp(),feeMod.getActualFeeOnPopUp());
+		feeMod.fillAddress();
+		feeMod.selectAmex();
+		Assert.assertEquals(feeMod.getInternetPlusTotalOnAddressPage(), feeMod.gettotalFeeOnAddressPage());
+		Assert.assertEquals(feeMod.gettotalFeeOnAddressPage(),feeMod.getTotalFeeOnPayBtn());
+		feeMod.clickOnPayBtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPayBtn(),feeMod.getTotalFeeOnPaymentGat());
+		feeMod.clickOncancelBtn();
+
+	}
+	
+	/**
+	 * Verify fee pay fee Login With Student amd parent  SelectedByAmex login by selecting Amex card on Web, Android and iOS
+	 * @author vinay kumar 
+	 * @since 2018-09-30
+	 * @version 1.2
+	 * @throws Throwable 
+	 */
+	@Test(dataProvider = "group3")
+	public void payLoginWithStudentSelectedByAmex(String role) throws IOException, InterruptedException {
+		base.readData(platform, "Guest", schoolName);
+		logMod.Login(role);
+		feeMod.clickOnPayfeeMod();
+		feeMod.selectSchoolToPayFee("Qa dashboard testing");
+		feeMod.enterAdmission("1994");
+		feeMod.clickOnProceedbtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPopUp(),feeMod.getActualFeeOnPopUp());
+		feeMod.fillAddress();
+		feeMod.selectAmex();
+		Assert.assertEquals(feeMod.getInternetPlusTotalOnAddressPage(), feeMod.gettotalFeeOnAddressPage());
+		Assert.assertEquals(feeMod.gettotalFeeOnAddressPage(),feeMod.getTotalFeeOnPayBtn());
+		feeMod.clickOnPayBtn();
+		Assert.assertEquals(feeMod.getTotalFeeOnPayBtn(),feeMod.getTotalFeeOnPaymentGat());
+		feeMod.clickOncancelBtn();
+
+	}
+}
