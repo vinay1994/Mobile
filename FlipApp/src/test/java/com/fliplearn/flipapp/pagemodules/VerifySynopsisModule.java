6e181aa07aca99ca39e25aabb5289e9782490866
Jagrati Mishra <jagratimishra21@gmail.com>
2018-10-23 19:00:14 +0530

    synopsis file

---
+++ FlipApp/src/test/java/com/fliplearn/flipapp/pagemodules/VerifySynopsisModule.java
@@ -0,0 +1,18 @@
+package com.fliplearn.flipapp.pagemodules;
+
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.FindBy;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.iOSFindBy;
+
+public class VerifySynopsisModule {
+	
+	@FindBy(xpath="")
+	@AndroidFindBy()
+	@iOSFindBy()
+	public RemoteWebElement claConMod;
+	
+	
+
+}
