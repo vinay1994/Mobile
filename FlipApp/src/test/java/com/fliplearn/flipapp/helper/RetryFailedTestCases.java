a965c099ed9a6fff3a48ae7d0663211f464dc787
tgoswami <goswami.tarun77@gmail.com>
2019-12-20 14:58:43 +0530

    fixed scripts

--- FlipApp/src/test/java/com/fliplearn/flipapp/helper/RetryFailedTestCases.java
+++ FlipApp/src/test/java/com/fliplearn/flipapp/helper/RetryFailedTestCases.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class RetryFailedTestCases implements IRetryAnalyzer {
     private int retryCnt = 0;
     //You could mentioned maxRetryCnt (Maximiun Retry Count) as per your requirement. Here I took 2, If any failed testcases then it runs two times
-    private int maxRetryCnt = 2;
+    private int maxRetryCnt = 1;
     
     //This method will be called everytime a test fails. It will return TRUE if a test fails and need to be retried, else it returns FALSE
     public boolean retry(ITestResult result) {
